e31670f04b1a4f9f37bcbce87d93b1a830972803
Meshack <mbuvi.meshack99@gmail.com>
2022-10-17 12:45:33 +0300

    Done

---
+++ app.rb
@@ -0,0 +1,18 @@
+# using puts 
+puts "Hello World!"
+# puts "Hello world!"
+# puts "Hello world!"
+
+
+ # using print
+ print "Pass this test, please."
+#  print "Hello world!"
+#  print "Hello world!"
+
+ #using p
+ p [1,2,3]
+
+
+#using pp
+
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
